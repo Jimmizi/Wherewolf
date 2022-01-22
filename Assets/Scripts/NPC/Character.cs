7cf1a1f451ff923b8cfe443bdbcb9cfeb02e9b81
Mizicoca <50930532+Mizicoca@users.noreply.github.com>
2022-01-22 12:47:29 +0000

    A start on information systems and emotes

---
+++ Assets/Scripts/NPC/Character.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    enum HairStyles
+    {
+        Long,
+        Medium,
+        Short,
+        Bald
+    }
+
+    enum HairColor
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
