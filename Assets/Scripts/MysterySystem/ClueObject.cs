fb44df8c04a25ffacc72b7a42be610678745fc4a
Mizicoca <50930532+Mizicoca@users.noreply.github.com>
2022-01-25 22:38:29 +0000

    update clueobject

--- Assets/Scripts/MysterySystem/ClueObject.cs
+++ Assets/Scripts/MysterySystem/ClueObject.cs
@@ -7,18 +7,18 @@ using UnityEngine;
 /// </summary>
 public class ClueObject
 {
-    public Character RelatesToCharacter;
-    public Character GivenByCharacter;
-
-    // What day was the clue given on
-    public int GivenOnDay = 0;
-
-    // Was it daytime when given the clue
-    public bool GivenDuringDay;
-
     // The list of emotes to form the sentence.
     public List<Emote> Emotes;
 
+    public Character GivenByCharacter;
+    public Character RelatesToCharacter;
+
     // Whether the string of emotes relates to a true statement.
     public bool IsTruth;
+
+    // What day was the clue given on
+    public int Day = 0;
+
+    // Time of day the clue was given
+    public WerewolfGame.TOD TimeOfDay;
 }
