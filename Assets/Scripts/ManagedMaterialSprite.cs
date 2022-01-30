429860a809b437e748e88ef271b4049e5a1ea9f7
nltf <nn97s9@gmail.com>
2022-01-30 17:33:11 +0100

    Move the grass

--- Assets/Scripts/ManagedMaterialSprite.cs
+++ Assets/Scripts/ManagedMaterialSprite.cs
@@ -1,27 +1,62 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class ManagedMaterialSprite : MonoBehaviour {
-
+    [Header("Sway")]
+    public bool Sway = false;
+    public float MaxSway = 0.1f;
+    public float Rigidness = 1f;
+    
     private SpriteRenderer _spriteRenderer;
     private Material _material;
 
+    private bool _materialChanged;
+    private MaterialPropertyBlock _materialPropertyBlock;
+    
     private void Awake() {
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        ApplyMaterialPropertyBlock();
     }
-    private void Start()
-    {
+
+    private void Start() {
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        ApplyMaterialPropertyBlock();
+    }
+
+    private void ApplyMaterialPropertyBlock() {
+        /* If there's no swaying, do not apply any material property block. */
+        if (_materialPropertyBlock == null && !Sway) return;
+        
+        if (_materialPropertyBlock == null) {
+            _materialPropertyBlock = new MaterialPropertyBlock();
+            if (_spriteRenderer.HasPropertyBlock()) {
+                _spriteRenderer.GetPropertyBlock(_materialPropertyBlock);
+            }
+        }
+
+        _materialPropertyBlock.SetFloat("_Sway", (Sway) ? 1f : 0f);
+        _materialPropertyBlock.SetFloat("_SwayMax", MaxSway);
+        _materialPropertyBlock.SetFloat("_Rigidness", Rigidness);
+        _spriteRenderer.SetPropertyBlock(_materialPropertyBlock);
     }
 
     public void SetMaterial(Material material) {
         _material = material;
 
-        if(!_spriteRenderer)
-        {
+        if (!_spriteRenderer) {
             _spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
         _spriteRenderer.sharedMaterial = material;
+        _materialChanged = true;
+        ApplyMaterialPropertyBlock();
+    }
+
+    private void Update() {
+        if (_materialChanged) {
+            ApplyMaterialPropertyBlock();
+            _materialChanged = false;
+        }
     }
 }
